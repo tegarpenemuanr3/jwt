e6bc7603292bdcaabb25f6a5059a3a6ac872f032
tegar <tegar@aksaramaya.com>
2023-11-03 08:31:27 +0700

    Setup server and logger

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-function startServer() {}
+const express = require("express");
+const logger = require("./logger");
+const app = express();
+const port = process.env.PORT || 3000;
+
+function startServer() {
+  app.use(express.json());
+
+  app.listen(port, () => {
+    logger.info(`Server listening at http://localhost:${port}`);
+  });
+}
 
 module.exports = startServer;
